79bf7537b05d881011b262dffd73592cbdb86547
cipta <ciphwn@gmail.com>
2017-04-30 02:17:14 -0400

    continue

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
+from time import clock
 import os
 
 import models.dcgan as dcgan
@@ -26,16 +27,21 @@ parser.add_argument('--nc', type=int, default=3, help='input image channels')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--nconv', type=int, default=64)
+parser.add_argument('--nef', type=int, default=64)
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--nstart', type=int, default=25, help='number of startup generator runs')
 parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.00005')
 parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.00005')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netE', default='', help="path to netE (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 parser.add_argument('--clamp_lower', type=float, default=-0.01)
 parser.add_argument('--clamp_upper', type=float, default=0.01)
 parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
+parser.add_argument('--Diter_plus', type=int, default=200, help='number of D iters per each G iter for "large" phases')
 parser.add_argument('--noBN', action='store_true', help='use batchnorm or not (only for DCGAN)')
 parser.add_argument('--mlp_G', action='store_true', help='use MLP for G')
 parser.add_argument('--mlp_D', action='store_true', help='use MLP for D')
@@ -68,6 +74,17 @@ if opt.dataset in ['imagenet', 'folder', 'lfw']:
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
+elif opt.dataset == 'flowers':
+    # folder dataset
+    dataset = dset.ImageFolder(root=opt.dataroot,
+                               transform=transforms.Compose([
+                                   transforms.Scale(int(opt.imageSize * 1.5)),
+                                   transforms.RandomCrop(opt.imageSize),
+                                   transforms.RandomHorizontalFlip(),
+                                   transforms.RandomVerticalFlip(),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
 elif opt.dataset == 'lsun':
     dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
                         transform=transforms.Compose([
@@ -91,6 +108,8 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
 nz = int(opt.nz)
 ngf = int(opt.ngf)
 ndf = int(opt.ndf)
+nconv = int(opt.nconv)
+nef = int(opt.nef)
 nc = int(opt.nc)
 n_extra_layers = int(opt.n_extra_layers)
 
@@ -109,41 +128,74 @@ if opt.netG != '': # load checkpoint if needed
     netG.load_state_dict(torch.load(opt.netG))
 print(netG)
 
-netE = dcgan.DCGAN_E(opt.imageSize, nz, nc, ndf, n_extra_layers)
+netE = dcgan.DCGAN_E(opt.imageSize, nz, nc, nef, n_extra_layers)
+netE.apply(weights_init)
 if opt.netE != '':
     netE.load_state_dict(torch.load(opt.netE))
 print(netE)
 
-netD = mlp.MLP_D(opt.imageSize, nc, netE.cndf, ndf)
+nx = opt.imageSize**2 * nc
+if opt.mlp_D:
+    #print('using MLP discriminator')
+    netD = mlp.MLP_D(nx + netE.nz, ndf)
+else:
+    # Use a DCGAN to turn the image to a vector, then compare against z.
+    print('using DCNN->MLP discriminator')
+    netD = mlp.MLP_ED(opt.imageSize, nz, nc, ndf, nconv=nconv)
+    netD.netE.apply(weights_init)
+    netD.netG.apply(weights_init)
+if opt.netD != '':
+    netD.load_state_dict(torch.load(opt.netD))
 print(netD)
 
 input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
 noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
+# Fixed z-vector that we can compare as training progresses.
 fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
+
+ridx = range(len(dataset))
+random.shuffle(ridx)
+fixed_image = next(iter(dataloader))[0]
+vutils.save_image(fixed_image.mul(0.5).add(0.5),
+    '{0}/fixed_image.png'.format(opt.experiment))
 one = torch.FloatTensor([1])
 mone = one * -1
 
 if opt.cuda:
+    print('Moving models to GPU')
     netD.cuda()
     netG.cuda()
     netE.cuda()
     input = input.cuda()
     one, mone = one.cuda(), mone.cuda()
     noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+    fixed_image = fixed_image.cuda()
 
 # setup optimizer
 if opt.adam:
     optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999))
-    optimizerG = optim.Adam(netG.parameters() + netE.parameters(),
+    optimizerG = optim.Adam(list(netG.parameters()) + list(netE.parameters()),
             lr=opt.lrG, betas=(opt.beta1, 0.999))
 else:
     optimizerD = optim.RMSprop(netD.parameters(), lr = opt.lrD)
-    optimizerG = optim.RMSprop(netG.parameters() + netE.parameters(), lr = opt.lrG)
+    optimizerG = optim.RMSprop(list(netG.parameters()) + list(netE.parameters()),
+            lr = opt.lrG)
+
+def rcpu(dataloader):
+    for data in dataloader:
+        real_cpu, _ = data
+        if opt.cuda:
+            real_cpu = real_cpu.cuda()
+        yield real_cpu
 
 gen_iterations = 0
+print('Starting training')
+past = clock()
 for epoch in range(opt.niter):
-    data_iter = iter(dataloader)
+    print('Starting epoch:', epoch)
+    data_iter = rcpu(dataloader)
     i = 0
+    redo = 0
     while i < len(dataloader):
         ############################
         # (1) Update D network
@@ -152,10 +204,14 @@ for epoch in range(opt.niter):
             p.requires_grad = True # they are set to False below in netG update
 
         # train the discriminator Diters times
-        if gen_iterations < 25 or gen_iterations % 500 == 0:
-            Diters = 100
+        if gen_iterations == 0:
+            Diters = 1 # Self-test
+        elif redo > 0 or gen_iterations < opt.nstart or gen_iterations % 500 == 0:
+            Diters = opt.Diter_plus
         else:
             Diters = opt.Diters
+        if gen_iterations > 15000:
+            Diters += 1
         j = 0
         while j < Diters and i < len(dataloader):
             j += 1
@@ -164,20 +220,17 @@ for epoch in range(opt.niter):
             for p in netD.parameters():
                 p.data.clamp_(opt.clamp_lower, opt.clamp_upper)
 
-            data = data_iter.next()
+            real = data_iter.next()
             i += 1
 
             # train with real
-            real_cpu, _ = data
             netD.zero_grad()
-            batch_size = real_cpu.size(0)
-
-            if opt.cuda:
-                real_cpu = real_cpu.cuda()
-            input.resize_as_(real_cpu).copy_(real_cpu)
+            batch_size = real.size(0)
+            input.resize_as_(real).copy_(real)
             inputz = Variable(netE(Variable(input, volatile=True)).data)
             inputv = Variable(input)
 
+            # Encoded z, Real x
             errD_real = netD(inputz, inputv)
             errD_real.backward(one)
 
@@ -185,12 +238,22 @@ for epoch in range(opt.niter):
             noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
             noisev = Variable(noise, volatile = True) # totally freeze netG
             fake = Variable(netG(noisev).data)
-            inputz = Variable(noise)
+            inputz = Variable(noise).squeeze()
             inputv = fake
+            # Real z, Decoded x
             errD_fake = netD(inputz, inputv)
             errD_fake.backward(mone)
             errD = errD_real - errD_fake
             optimizerD.step()
+
+        # Hack to keep training critic if distance too low.
+        if redo < 10 and errD.abs().data[0] < 0.000001:
+            if redo == 0:
+                print(errD.abs().data[0])
+            redo += 1
+            continue
+        if redo > 0:
+            redo = -3
 
         if i == len(dataloader):
             break
@@ -207,34 +270,48 @@ for epoch in range(opt.niter):
         noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
         noisev = Variable(noise)
         fake = netG(noisev)
-        errG = netD(fake)
+        # Real z, Decoded x
+        errG = netD(noisev.squeeze(), fake)
         errG.backward(one)
 
-        data = data_iter.next()
+        real = data_iter.next()
         i += 1
-            # train with real
-        real_cpu, _ = data
-        batch_size = real_cpu.size(0)
-        if opt.cuda:
-            real_cpu = real_cpu.cuda()
-        input.resize_as_(real_cpu).copy_(real_cpu)
+        # train with real
+        batch_size = real.size(0)
+        input.resize_as_(real).copy_(real)
         inputv = Variable(input)
-        errE = netE(inputv)
+        fakez = netE(inputv)
+        # Encoded z, Real x
+        errE = netD(fakez, inputv)
         errE.backward(mone)
 
+        errB = errG - errE
         optimizerG.step()
         gen_iterations += 1
 
-        print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
-            % (epoch, opt.niter, i, len(dataloader), gen_iterations,
-            errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
-        if gen_iterations % 500 == 0:
-            real_cpu = real_cpu.mul(0.5).add(0.5)
-            vutils.save_image(real_cpu, '{0}/real_samples.png'.format(opt.experiment))
+        if gen_iterations < opt.nstart or gen_iterations % 50 == 0:
+            now = clock()
+            elapsed = now - past
+            print('[%d/%d][%d/%d][%d][%.2fs] Loss_D: %f Loss_B: %f'
+                % (epoch, opt.niter, i, len(dataloader), gen_iterations, elapsed,
+                errD.data[0], errB.data[0]))
+            past = now
+        if gen_iterations == 1 or gen_iterations % 500 == 0:
+            # Generate reconstructions of real samples as well as
+            # Images from a fixed noise vector.
             fake = netG(Variable(fixed_noise, volatile=True))
             fake.data = fake.data.mul(0.5).add(0.5)
-            vutils.save_image(fake.data, '{0}/fake_samples_{1}.png'.format(opt.experiment, gen_iterations))
+            vutils.save_image(fake.data, '{0}/fake_samples_{1}.png'.format(
+                opt.experiment, gen_iterations))
+
+            real_z = netE(Variable(fixed_image, volatile=True))
+            reconst = netG(real_z.unsqueeze(2).unsqueeze(3))
+            reconst.data = reconst.data.mul(0.5).add(0.5)
+            vutils.save_image(reconst.data, '{0}/reconstruction_samples_{1}.png'.format(
+                opt.experiment, gen_iterations))
 
     # do checkpointing
-    torch.save(netG.state_dict(), '{0}/netG_epoch_{1}.pth'.format(opt.experiment, epoch))
-    torch.save(netD.state_dict(), '{0}/netD_epoch_{1}.pth'.format(opt.experiment, epoch))
+    if epoch % 10 == 0:
+        torch.save(netG.state_dict(), '{0}/netG_epoch_{1}.pth'.format(opt.experiment, epoch))
+        torch.save(netE.state_dict(), '{0}/netE_epoch_{1}.pth'.format(opt.experiment, epoch))
+        torch.save(netD.state_dict(), '{0}/netD_epoch_{1}.pth'.format(opt.experiment, epoch))
--- models/mlp.py
+++ models/mlp.py
@@ -4,16 +4,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import torch
 import torch.nn as nn
+import models.dcgan as dg
 
 
 class MLP_D(nn.Module):
-    def __init__(self, isize, nc, nz, ndf, ngpu):
+    def __init__(self, n, ndf):
         super(MLP_D, self).__init__()
-        self.ngpu = ngpu
 
         main = nn.Sequential(
             # Z goes into a linear of size: ndf
-            nn.Linear(nc * isize * isize + nz, ndf),
+            nn.Linear(n, ndf),
             nn.ReLU(True),
             nn.Linear(ndf, ndf),
             nn.ReLU(True),
@@ -22,13 +22,30 @@ class MLP_D(nn.Module):
             nn.Linear(ndf, 1),
         )
         self.main = main
-        self.nc = nc
-        self.isize = isize
 
-    def forward(self, inputz, inputv):
-        inputv = inputv.view(inputv.size(0), -1)
-        input = torch.cat([inputz, inputv], 0)
-
+    def forward(self, input):
         output = self.main(input)
         output = output.mean(0)
         return output.view(1)
+
+class MLP_ED(nn.Module):
+    '''Combines DCGAN and MLP.'''
+    def __init__(self, isize, nz, nc, ndf, nconv=0):
+        super(MLP_ED, self).__init__()
+
+        if nconv == 0:
+            nconv = ndf
+
+        self.netG = dg.DCGAN_G(isize, nz, nc, nconv)
+        self.netE = dg.DCGAN_E(isize, nz, nc * 2, nconv, linear_growth=True)
+        self.pool = nn.AvgPool2d(2)
+
+        all_dim = 2 * nz
+        self.mlp = MLP_D(all_dim, ndf)
+        self.nz = nz
+
+    def forward(self, z, x):
+        xt = self.netG(z.unsqueeze(2).unsqueeze(3))
+        zt = self.netE(torch.cat([x, xt], 1))
+        all_ins = torch.cat([z, zt], 1)
+        return self.mlp(all_ins)
